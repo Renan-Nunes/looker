4f7f5eb2181ebebe16815deacf10fd1b7e8e7443
Renan <renannuneslima21@gmail.com>
2025-08-03 23:41:39 -0300

    add user model

--- app/models/models_user.py
+++ app/models/models_user.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Column, Integer, String, Date
-from app.core.database import Base
+from core.db_usuario import Base
 
 class Usuario(Base):
     __tablename__ = "usuario"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(80), nullable=False)
-    cpf = Column(String(11), unique=True)
+    nome = Column(String(100), nullable=False)
+    cpf = Column(String(11), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     senha = Column(String(255), nullable=False)
     telefone = Column(String(20))
