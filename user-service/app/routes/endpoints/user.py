2ae529c98a9f678fff8a92edce4f7a1cd07dd8f2
Renan <renannuneslima21@gmail.com>
2025-08-25 22:19:54 -0300

    fix

--- user-service/app/routes/endpoints/user.py
+++ user-service/app/routes/endpoints/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 from typing import List
 from passlib.context import CryptContext
