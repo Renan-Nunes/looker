dbfef3e398b99c8babd71bd67ff974ccbaf036df
Renan <renannuneslima21@gmail.com>
2025-08-27 00:37:50 -0300

    CORRECAO EM TD DO AFFONSO

--- alugueis-service/app/core/config.py
+++ alugueis-service/app/core/config.py
@@ -1,20 +1,18 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 from typing import Optional
-from dotenv import load_dotenv  
-import os
 
-load_dotenv()
-os.getenv("DATABASE_URL")
-os.getenv("FILMES_SERVICE_URL")
-os.getenv("USUARIOS_SERVICE_URL")
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_PATH = BASE_DIR / ".env"
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     FILMES_SERVICE_URL: Optional[str] = None
     USUARIOS_SERVICE_URL: Optional[str] = None
-    
+
     class Config:
-        env_file = ".env"
+        env_file = ENV_PATH
+        env_file_encoding = "utf-8"
 
 settings = Settings()
+print(f"DATABASE_URL = {settings.DATABASE_URL}")
--- filmes-service/app/models/__init__.py
+++ filmes-service/app/models/__init__.py
@@ -1,1 +1,2 @@
 from .filme import Filme
+from .review import Review
