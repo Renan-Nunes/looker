9c4e079205b5370342b266d0f9ff9721b31750e6
Renan <renannuneslima21@gmail.com>
2025-08-29 14:39:23 -0300

    fix

--- alugueis-service/app/core/database.py
+++ alugueis-service/app/core/database.py
@@ -1,16 +1,33 @@
+import os
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from .config import settings  # precisa do config ajustado
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+# Detecta se está rodando como exe
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Carrega o .env
+env_path = os.path.join(base_path, ".env")
+load_dotenv(env_path)
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
+DB_NAME = os.getenv("DB_NAME", "trabalho_if")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db: Session = SessionLocal()
     try:
         yield db
     finally:
