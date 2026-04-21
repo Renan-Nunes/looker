2c78aeb2d2afd1c2a371400616e8be6a0c6ad41f
pepe <pedrocastilho72@hotmail.com>
2026-04-21 18:50:26 -0300

    feat: refactor header with smart hide and glassmorphism

--- frontend/looker/src/shared/header/header.spec.ts
+++ frontend/looker/src/shared/header/header.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { Header } from './header';
+import { Router, provideRouter } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { LoginService } from '../../services/login-service';
 
 describe('Header', () => {
   let component: Header;
@@ -10,7 +11,11 @@ describe('Header', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Header],
-      providers: [provideRouter([]), provideHttpClient()],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        provideRouter([]),
+        { provide: LoginService, useValue: jasmine.createSpyObj('LoginService', ['authenticate', 'register']) }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Header);
@@ -18,7 +23,31 @@ describe('Header', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { expect(component).toBeTruthy(); });
+
+  it('should start with scrolled = false', () => {
+    expect(component.scrolled).toBeFalse();
+  });
+
+  it('should set scrolled = true when scrollY > 40', () => {
+    component.onScroll({ scrollTop: 50 } as any);
+    expect(component.scrolled).toBeTrue();
+  });
+
+  it('should hide header when scrolling down past threshold', () => {
+    component.onScroll({ scrollTop: 100 } as any);
+    component.onScroll({ scrollTop: 200 } as any);
+    expect(component.hidden).toBeTrue();
+  });
+
+  it('should show header when scrolling up', () => {
+    component.onScroll({ scrollTop: 200 } as any);
+    component.onScroll({ scrollTop: 100 } as any);
+    expect(component.hidden).toBeFalse();
+  });
+
+  it('should open login modal when openLogin() is called', () => {
+    component.openLogin();
+    expect(component.loginVisible).toBeTrue();
   });
 });
